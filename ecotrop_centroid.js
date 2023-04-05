4913778fdec3a031828d8dcc0b74ff1d6ae1eccb
LyubeznayaVeronika <kelertyr@gmail.com>
2023-04-05 14:16:47 +0300

    add files

---
+++ ecotrop_centroid.js
@@ -0,0 +1,83 @@
+var eco1 = {
+    "type": "FeatureCollection",
+    "name": "eco1",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "id": 1, "Назва": "Цнянское водохранили" }, "geometry": { "type": "Point", "coordinates": [ 27.583227804913598, 53.961964284794995 ] } }
+    ]}
+
+var eco2 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 2, "Назва": "Чижовка" }, "geometry": { "type": "Point", "coordinates": [ 27.643452720530792, 53.848318626839834 ] } },
+    ]}
+var eco3 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 3, "Назва": "Малявки" }, "geometry": { "type": "Point", "coordinates": [ 27.609746712358501, 53.845693574953287 ] } },
+    ]}
+var eco4 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 4, "Назва": "Курасовщина" }, "geometry": { "type": "Point", "coordinates": [ 27.513588550268583, 53.849392321037513 ] } },
+    ]}
+
+var  eco5 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 5, "Назва": "Лесное озеро" }, "geometry": { "type": "Point", "coordinates": [ 27.665877950934476, 53.906948672219606 ] } },
+    ]}
+
+var eco6 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 6, "Назва": "Белая дача" }, "geometry": { "type": "Point", "coordinates": [ 27.508153848973496, 53.852634852457165 ] } }
+    ]}
+
+var eco7 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 7, "Назва": "Ботанический сад" }, "geometry": { "type": "Point", "coordinates": [ 27.61246777326177, 53.915051598458845 ] } },
+    ]}
+
+var eco8 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 8, "Назва": "Птичья полянка" }, "geometry": { "type": "Point", "coordinates": [ 27.428234549281651, 53.893240739166302 ] } },
+    ]}
+
+var eco9 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 9, "Назва": "Город птиц" }, "geometry": { "type": "Point", "coordinates": [ 27.580483495614214, 53.864921849817506 ] } },
+    ]}
+
+var eco10 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 10, "Назва": "Серебряный лог" }, "geometry": { "type": "Point", "coordinates": [ 27.593024644480632, 53.853561989687172 ] } },
+    ]}
+
+var eco11 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 11, "Назва": "Медвежино" }, "geometry": { "type": "Point", "coordinates": [ 27.475358578785034, 53.892347078635169 ] } },
+    ]}
+
+var eco12 = {
+    "type": "FeatureCollection",
+    "name": "ecotrop_centroid",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [{ "type": "Feature", "properties": { "id": 12, "Назва": "Яблоневый сад" }, "geometry": { "type": "Point", "coordinates": [ 27.485829449105772, 53.875597702383097 ] } },
+    ]}
+
